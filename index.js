3a8c8b3c3009ceacc91ca2ca627bf8b559e67ab6
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-21 14:52:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Removing non-alphanumeric characters and convert to lowercase
+  word = word.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+  // Comparing the original word with its reverse
+  return word === word.split('').reverse().join('');
 }
 
-/* 
-  Add your pseudocode here
-*/
+// Tests
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
 
-/*
-  Add written explanation of your solution here
-*/
+console.log("");
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
 
 module.exports = isPalindrome;
+
+// run node index.js
